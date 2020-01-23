7dd796e3f1be9057d4ee16c65be2d037ac63725f
asal <asal.react@gmail.com>
2020-01-23 14:30:12 -0500

    created burger.css and did code in burger.js

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
+
+const burger = (props) => {
+    return( 
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top"/>
+        </div>
+
+    );
+};
+
+export default burger;
