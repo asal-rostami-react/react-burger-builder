774c6fb77cb78f636036f05a9bb9b803fc95251e
asal <asal.react@gmail.com>
2020-01-23 14:44:14 -0500

    some changes

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,6 +6,9 @@ const burger = (props) => {
     return( 
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top"/>
+            <BurgerIngredient type="cheese"/>
+            <BurgerIngredient type="meat"/>
+            <BurgerIngredient type="bread-bottom"/>
         </div>
 
     );
