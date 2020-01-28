bd4a77a5a1f258d6f7e929dca049664172ede606
asal <asal.react@gmail.com>
2020-01-28 16:14:15 -0500

    created Model for summery

---
+++ src/components/UI/Model/Model.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import classes from './Model.css';
+
+const model = (props) => (
+    <div className={classes.Model}>
+        {props.children}
+    </div>
+);
+
+export default model;
