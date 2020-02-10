b0e0f4bdc8cee11d9e39a6cc87843afb683480dd
asal <asal.react@gmail.com>
2020-02-10 16:40:05 -0500

    added erroHandler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Model from '../../components/UI/Model/Model';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -157,4 +158,4 @@ class BurgerBuilder extends Component {
 
     }
 }
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
