56908410a5a521502cb3eeada04a629727ca208b
asal <asal.react@gmail.com>
2020-02-10 15:17:21 -0500

    added spinner to project

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Model from '../../components/UI/Model/Model';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -25,7 +26,8 @@ class BurgerBuilder extends Component {
             },
             totalPrice: 4,
             purchasable: false,
-            purchasing: false
+            purchasing: false,
+            loading: false
 
         }
         
@@ -82,6 +84,7 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
+        this.setState({loading: true});
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice.toFixed(2),
@@ -99,15 +102,20 @@ class BurgerBuilder extends Component {
         }
 
         axios.post('/orders.json',order)
-            .then(response => console.log(response))
-            .catch(error => console.log(error));
+            .then(response =>  {
+                this.setState({loading: false, purchasing: false});
+            })
+            .catch(error => {
+                this.setState({loading: false, purchasing: false});
+            });
     }
 
     purchaseHandler = () => {
         this.setState({purchasing:true});
     }
-    render(){
 
+    
+    render(){
         // make a copy of ingredients and check if the number of given ingredient is 0 or less
         // , change value to false
         const disabledInfo = {
@@ -118,14 +126,21 @@ class BurgerBuilder extends Component {
             // {salad:true , bacon:false, cheese:false, meat: true}
         }
         console.log("disabled",disabledInfo);
+
+        let orderSummary = <OrderSummary 
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        purchaseCanceled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler}/>;
+
+        if(this.state.loading){
+            orderSummary = <Spinner />;
+        
+        }
         return(
             <Aux>
                 <Model show={this.state.purchasing} modelClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchaseCanceled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler}/>
+                    {orderSummary}
                 </Model>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls 
