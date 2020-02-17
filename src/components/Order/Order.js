cc5b8970fe706476dca1012a6dd03f3b30ef976c
asal <asal.react@gmail.com>
2020-02-17 15:58:31 -0500

    added order to orders and app

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import classes from './Order.css';
 
 const order = () => (
-    <div>
+    <div className={classes.Order}>
         <p>Ingredients: Salad (1)</p>
         <p>Price: <strong>CAD 5.45</strong></p>
     </div>
 );
 
-export default order;
+export default order; 
