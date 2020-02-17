2bb6092755d1f34ba7be2d57d70b8a4160bc11ad
asal <asal.react@gmail.com>
2020-02-17 09:39:42 -0500

    added switch to route

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
-import {Route} from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 
 
 
@@ -11,8 +11,11 @@ class App extends Component {
     return (
       <div>
         <Layout>
-           <Route path="/" exact component={BurgerBuilder} />
-           <Route path="/checkout" component={Checkout} />
+          <Switch> 
+            <Route path="/checkout" component={Checkout} />
+            <Route path="/" component={BurgerBuilder} />
+          </Switch>
+           
         </Layout>
        
       </div>
