5c712cbf94b3ee6501c8ffea59db1e4ed59937a4
asal <asal.react@gmail.com>
2020-02-17 09:38:08 -0500

    applied router to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import {Route} from 'react-router-dom'
 
 
 
@@ -10,8 +11,8 @@ class App extends Component {
     return (
       <div>
         <Layout>
-           <BurgerBuilder />
-           <Checkout />
+           <Route path="/" exact component={BurgerBuilder} />
+           <Route path="/checkout" component={Checkout} />
         </Layout>
        
       </div>
