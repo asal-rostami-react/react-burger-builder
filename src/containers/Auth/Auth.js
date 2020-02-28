8fa686899e7993cc09b31a80eaf31b02cc77a857
asal <asal.react@gmail.com>
2020-02-28 13:39:29 -0500

    added style to Auth comp

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 
 import Input from '../../components/UI/Input/Input';
 import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
 
 class Auth extends Component {
 
@@ -58,7 +59,7 @@ class Auth extends Component {
 
         ));
         return (
-            <div>
+            <div className= {classes.Auth}>
                 <form>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
