70aa0bbf40ed4b893c896d047f5ff2de9be5c96c
asal <asal.react@gmail.com>
2020-03-03 16:48:04 -0500

    set out app with localstorage

--- src/App.js
+++ src/App.js
@@ -6,11 +6,17 @@ import Orders from './containers/Orders/Orders';
 import { Route, Switch } from 'react-router-dom';
 import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
+import { connect } from 'react-redux';
+import * as actions from './store/actions/index';
 
 
 
 
 class App extends Component {
+componentDidMount() {
+  this.props.onTryAutoSignup();
+}
+
   render() {
     return (
       <div>
@@ -30,4 +36,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
