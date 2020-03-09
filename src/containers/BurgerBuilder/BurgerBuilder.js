d3c041597f66c6b2c3b6f8b23ed525a5ff580b00
asal <asal.react@gmail.com>
2020-03-09 10:04:35 -0400

    finished project

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -13,7 +13,7 @@ import * as actions from '../../store/actions/index';
 import thunk from 'redux-thunk';
 
 
-class BurgerBuilder extends Component {
+export class BurgerBuilder extends Component {
 
     constructor(props) {
         super(props);
